05e9d2892d34f52ee0e2f7670b0773d775fa1422
Pamela Frieze <pfrieze@gmail.com>
2017-10-16 20:24:58 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -12,9 +12,9 @@ def shipping_manifest
   #fill that hash with key/value pairs that describe the following information:
   #We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings
 shipping_manifest = {
-"whale_bone_corsets" => '5',
-"porcelain_vases" => '2',
-"oil_vases" => '3'
+"whale_bone_corsets" => 5,
+"porcelain_vases" => 2,
+"oil_vases" => 3
 }
 end
 
