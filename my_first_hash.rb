5367df1ef9af4b420cf965923cb126677e096529
Pamela Frieze <pfrieze@gmail.com>
2017-10-16 20:21:37 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,6 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-pam_info = {"my_name" => "Pam"
+pam_info = {"my_name" => "Pam",
 "my_town" => "Somerville"}
 end
 
