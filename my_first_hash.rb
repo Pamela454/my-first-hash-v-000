b00e59d2ecc070654ca3a59b3ab44f46686151b5
Pamela Frieze <pfrieze@gmail.com>
2017-10-16 20:24:24 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -11,8 +11,11 @@ def shipping_manifest
   #set a variable called `shipping_manifest`, equal to a hash
   #fill that hash with key/value pairs that describe the following information:
   #We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings
-
-
+shipping_manifest = {
+"whale_bone_corsets" => '5',
+"porcelain_vases" => '2',
+"oil_vases" => '3'
+}
 end
 
 def retrieval
